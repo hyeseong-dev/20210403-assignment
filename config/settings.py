ee913b95201d3b3f86cf062d447c014722aa1886
hyeseong-lee <hyeseong43@gmail.com>
2021-04-03 14:00:55 +0900

    라인웍스 백엔드 엔지니어 과제 1. 환자와 방문 테이블들의 간단한 통계를 제공하는 API를 구현합니다. ● 환자  ○ 전체 환자 수  ○ 성별 환자 수  ○ 인종별 환자 수  ○ 민족별 환자 수  ○ 사망 환자 수
    ● 방문
     ○ 방문 유형(입원/외래/응급)별 방문 수
     ○ 성별 방문 수
     ○ 인종별 방문 수
     ○ 민족별 방문 수
     ○ 방문시 연령대(10세 단위)별 방문 수
    
    2. 각 테이블에 사용된 concept_id들의 정보를 얻을 수 있는 API를 만듭니다.
    다음 기능을 포함해야합니다.
    ● 검색 기능
     ○ 키워드 검색
    ● Pagination 기능
    
    3. 각 테이블의 row를 조회하는 API를 구현합니다.
    다음 기능을 포함해야합니다.
    ● concept id와 concept name 매칭
     ○ concept의 의미를 알 수 있게 이름을 함께 return합니다.
    ● Pagination 기능
    ● 특정 컬럼 검색 기능
     ○ 키워드 검색

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
+    'rest_framework',
     'corsheaders',
     'django_extensions',
     'linewalks',
@@ -36,7 +38,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
-    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
